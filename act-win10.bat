b05007fa3993d2c3d18854358598d1ab8bbd5547
nyuiop <gapin.sla@gmail.com>
2025-05-13 14:00:30 +0300

    Add files via upload

---
+++ act-win10.bat
@@ -0,0 +1,3 @@
+slmgr.vbs /ipk "7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH"
+slmgr.vbs /skms kms.digiboy.ir
+slmgr.vbs /ato
---
+++ monik.bat
@@ -0,0 +1,4 @@
+@echo off
+:loop
+"X:\Program Files\MonitorOff\MonitorOff_x64.exe" && cls
+goto loop
