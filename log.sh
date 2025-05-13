16a446dec83f5ea7b775d28d5297d081f63bad6d
nyuiop <gapin.sla@gmail.com>
2025-05-13 14:07:13 +0300

    Add files via upload

---
+++ log.sh
@@ -0,0 +1,25 @@
+sudo rm -f '/var/log/ufw.log.2.gz' '/var/log/ufw.log.3.gz' 
+sudo rm -f '/var/log/auth.log.2.gz' '/var/log/auth.log.3.gz' '/var/log/kern.log.2.gz' '/var/log/kern.log.3.gz' '/var/log/kern.log.4.gz' 
+sudo rm -f '/var/log/macchanger.log.1.gz' '/var/log/macchanger.log.2.gz' 
+sudo rm -f '/var/log/ufw.log.4.gz' 
+sudo rm -f /var/log/*.gz
+sudo rm -f /var/log/apt/*.gz
+sudo rm -f /var/log/istaller/*.log
+sudo rm -f /var/log/installer/*.log
+sudo rm -f /var/log/apt/*.xz
+sudo rm -f /var/log/apt/*.log
+sudo rm -f /var/log/*.log
+sudo rm -f /var/log/*.log1
+sudo rm -f /var/log/*.log/1
+sudo rm -f /var/log/*.log.1
+sudo rm -f /var/log/*.log.3
+sudo rm -f /var/log/*.log.4
+sudo rm -f /var/log/*.log.5
+sudo rm -f /var/log/*.log.6
+sudo rm -f /var/log/*.log.2
+sudo rm -f /var/log/unattended-upgrades/*.log
+sudo rm -f /var/log/unattended-upgrades/*.gz
+sudo rm -f /var/log/*.1
+sudo rm -f /var/log/*.0
+sudo rm -f /var/log/dmesg
+
